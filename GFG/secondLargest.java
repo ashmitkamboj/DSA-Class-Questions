5dbec884dc85359e8c265d6d3a515a739f0e00d0
Ashmit Kamboj <ashmitkamboj@gmail.com>
2025-07-29 23:53:22 +0530

    DSA

--- GFG/secondLargest.java
+++ GFG/secondLargest.java
@@ -1,31 +1,44 @@
-public class secondLargest 
-{
-    public float getSecondLargest(int[] arr) 
-    {
+import java.util.Scanner;
+public class SecondLargest {
+    public static int secondLargest(int[] arr){
         int n = arr.length;
-        float first = Float.NEGATIVE_INFINITY;
-        float second = Float.POSITIVE_INFINITY;
-        if (n < 2)
-        {
+        if (n < 2) {
             return -1;
-        } 
-        for(float num : arr)
-        {
-            if(num > first)
-            {
+        }
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+        for (int num : arr){
+            if (num > first) {
                 second = first;
                 first = num;
-            } else if (num > second && num != first)
-            {
+            } else if (num > second && num < first) {
                 second = num;
             }
         }
-        if (second == Float.POSITIVE_INFINITY)
-        {
+        if (second == Integer.MIN_VALUE) {
             return -1;
-        } else 
-        {
-            return second;
+        }
+        return second;
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Array must contain at least two elements.");
+            return;
+        }
+
+        int [] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int result = secondLargest(arr);
+        if (result == -1) {
+            System.out.println("No second largest element found.");
+        } else {
+            System.out.println("The second largest element is: " + result);
         }
     }
 }
